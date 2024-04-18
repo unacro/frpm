d1c51ae052849516effb93eea173c5cd324b662b
unacro <git@ews.ink>
2024-04-18 22:13:45 +0800

    fix: cp error at first run

--- prototype.sh
+++ prototype.sh
@@ -132,11 +132,11 @@ WantedBy = multi-user.target
 EOF
 }
 
-init_frp() {
-    # todo 有必要保留吗
+init() {
     mkdir -p "${FRP_INSTALL_DIR}"
     mkdir -p "${FRP_CACHE_DIR}"
     mkdir -p "${FRP_CONFIG_DIR}"
+    create_template
     if [ ! -f "${FRP_CONFIG_DIR}/frpc.toml" ]; then
         cp "${FRP_CONFIG_DIR}/template/frpc.example.toml" "${FRP_CONFIG_DIR}/frpc.toml"
     fi
@@ -145,19 +145,18 @@ init_frp() {
     fi
 }
 
-reset_frp() {
-    init_frp
+reset_config() {
     cp -f "${FRP_CONFIG_DIR}/template/frpc.example.toml" "${FRP_CONFIG_DIR}/frpc.toml"
     cp -f "${FRP_CONFIG_DIR}/template/frps.example.toml" "${FRP_CONFIG_DIR}/frps.toml"
     echo "已重设配置文件: ${FRP_CONFIG_DIR} 下的 frps.toml / frpc.toml"
 }
 
-check_frp_version() {
+check_version() {
     latest_release_info=$(curl -s "https://api.github.com/repos/${AUTHOR}/${REPO_NAME}/releases/latest")
     latest_release_version=$(echo "$latest_release_info" | jq -r '.tag_name' | sed s/^v//)
 }
 
-relink_frp() {
+relink() {
     latest_release_name=$(echo $latest_release_file | sed 's/\.tar\.gz$//')
     if [ -d "${FRP_INSTALL_DIR}/releases/${latest_release_name}" ]; then
         rm -rf "${FRP_INSTALL_DIR}/releases/${latest_release_name}"
@@ -172,8 +171,8 @@ relink_frp() {
     ln -s ${FRP_INSTALL_DIR}/frps ${FRP_BIN_PATH}/frps
 }
 
-upgrade_frp() {
-    check_frp_version
+upgrade() {
+    check_version
     download_url=$(echo "$latest_release_info" | jq -r ".assets | map(select(.name | contains(\"linux_$(dpkg --print-architecture)\"))) | .[0].browser_download_url")
     latest_release_file=$(basename $download_url)
     if [ -f "${FRP_CACHE_DIR}/${latest_release_file}" ]; then
@@ -182,14 +181,14 @@ upgrade_frp() {
         wget -P "${FRP_CACHE_DIR}" $download_url
     fi
     if [ -f "${FRP_CACHE_DIR}/${latest_release_file}" ]; then
-        relink_frp
+        relink
     else
         exit 1
     fi
     echo -e "Now \033[1;4;96mfrp\033[0m is \033[5;32mup to date\033[0m. Current version: \033[1;33m$(${FRP_INSTALL_DIR}/frpc -v)\033[0m"
 }
 
-install_frp_service() {
+install_service() {
     cp -f "${FRP_CONFIG_DIR}/template/frps.example.service" /etc/systemd/system/frps.service
     cp -f "${FRP_CONFIG_DIR}/template/frpc.example.service" /etc/systemd/system/frpc.service
     sudo systemctl daemon-reload
@@ -198,7 +197,7 @@ install_frp_service() {
     echo -e "- (退出本脚本后)运行 \033[1;36msystemctl enable frpc\033[0m(或者 frps) 启用\033[1;33m开机自启\033[0m"
 }
 
-configure_frpc() {
+configure_client() {
     if [ ! -f "/etc/systemd/system/frpc.service" ]; then
         cp "${FRP_CONFIG_DIR}/template/frpc.example.service" /etc/systemd/system/frpc.service
     fi
@@ -225,13 +224,13 @@ configure_frpc() {
     echo "已更新配置"
 }
 
-reload_frpc() {
+reload_client() {
     export FRPC_SERVER_ADDR="$(awk -F= '/FRPC_SERVER_ADDR/ {gsub(/"/,"",$3); print $3}' /etc/systemd/system/frpc.service)"
     export FRPC_SERVER_PORT="$(awk -F= '/FRPC_SERVER_PORT/ {gsub(/"/,"",$3); print $3}' /etc/systemd/system/frpc.service)"
     ${FRP_INSTALL_DIR}/frpc reload -c ${FRP_INSTALL_DIR}/frpc.toml
 }
 
-check_frpc() {
+check_client() {
     export FRPC_SERVER_ADDR="$(awk -F= '/FRPC_SERVER_ADDR/ {gsub(/"/,"",$3); print $3}' /etc/systemd/system/frpc.service)"
     export FRPC_SERVER_PORT="$(awk -F= '/FRPC_SERVER_PORT/ {gsub(/"/,"",$3); print $3}' /etc/systemd/system/frpc.service)"
     ${FRP_INSTALL_DIR}/frpc status -c ${FRP_INSTALL_DIR}/frpc.toml
@@ -244,7 +243,7 @@ main() {
     echo -e "一个简陋的 frp 工具脚本 v${SCRIPT_VERSION} by unacro"
     echo "frp - A Fast Reverse Proxy (https://gofrp.org)"
     echo "自动检查 frp 版本更新中..."
-    check_frp_version
+    check_version
     installed=$([[ -x "${FRP_INSTALL_DIR}/frpc" ]] && echo "\033[1;32m已安装\033[0m ($(${FRP_INSTALL_DIR}/frpc -v) $([[ "$(${FRP_INSTALL_DIR}/frpc -v)" = "${latest_release_version}" ]] && echo "\033[1;32m已是最新\033[0m" || echo "- \033[1;34m可以更新\033[0m ${latest_release_version}"))" || echo "\033[1;31m未安装\033[0m")
     echo -e "当前 frp 安装状态: ${installed}"
     frps_systemd_installed=$([[ -f "/etc/systemd/system/frps.service" ]] && echo "\033[1;32m已安装\033[0m" || echo "\033[1;33m未安装\033[0m")
@@ -253,36 +252,36 @@ main() {
     frps_running=$(pgrep -x "frps" >/dev/null && echo "\033[1;32m运行中\033[0m" || echo "\033[1;33m未运行\033[0m")
     frpc_running=$(pgrep -x "frpc" >/dev/null && echo "\033[1;32m运行中\033[0m" || echo "\033[1;33m未运行\033[0m")
     echo -e "当前 frp 运行状态: 服务端${frps_running} / 客户端${frpc_running}"
-    init_frp
+    init
     echo "${divider2}"
     options=(
+        "更新应用版本"
+        "安装系统服务"
         "修改配置文件"
         "重载配置文件"
         "查看代理状态"
-        "更新应用版本"
-        "安装系统服务"
         "重置配置文件"
         "退出当前脚本"
     )
     select choice in "${options[@]}"; do
         case $REPLY in
         1)
-            configure_frpc # 默认配置 frpc
+            upgrade
             ;;
         2)
-            reload_frpc # 默认重载 frpc
+            install_service
             ;;
         3)
-            check_frpc # 只有客户端可以使用 status 命令查看代理运行状态
+            configure_client # 默认配置 frpc
             ;;
         4)
-            upgrade_frp
+            reload_client # 默认重载 frpc
             ;;
         5)
-            install_frp_service
+            check_client # 只有客户端可以使用 status 命令查看代理运行状态
             ;;
         6)
-            reset_frp
+            reset_config
             ;;
         7)
             echo "886"
