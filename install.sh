d3ba07d7019d68217df9017c9e3abb8dacf66320
unacro <git@ews.ink>
2024-04-18 22:18:34 +0800

    fix: chmod +x make file in git became different

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 APP_PATH="/usr/local/share/frp"
 mkdir -p "${APP_PATH}"
 rm -f "${APP_PATH}/frpm"
-ln -s "${PWD}/prototype.sh" "${APP_PATH}/frpm"
+cat > "${APP_PATH}/frpm" << EOF
+#!/bin/bash
+
+bash ${PWD}/prototype.sh
+EOF
 chmod +x "${APP_PATH}/frpm"
 echo -e "frpm installed at \033[1;32m${APP_PATH}/frpm\033[0m"
