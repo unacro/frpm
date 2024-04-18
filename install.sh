c433f554f9166209b928be3916caa2c4f3e9de9f
unacro <git@ews.ink>
2024-04-18 21:58:57 +0800

    build: modify shortcut location

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-APP_PATH="/usr/local/bin/frpm"
-rm -f "${APP_PATH}"
-ln -s "${PWD}/prototype.sh" "${APP_PATH}"
-chmod +x "${APP_PATH}"
+APP_PATH="/usr/local/share/frp"
+mkdir -p "${APP_PATH}"
+rm -f "${APP_PATH}/frpm"
+ln -s "${PWD}/prototype.sh" "${APP_PATH}/frpm"
+chmod +x "${APP_PATH}/frpm"
